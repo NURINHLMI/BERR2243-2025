b3e70d7ec939f2b34a60ba48557793fa2f094cbb
NUrin Halmi <batrisyiahalmi@gmail.com>
2025-04-04 15:54:18 +0800

    Added Week01.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const db = client. db("testDB");
 const collection = db. collection ("users");
 
 // Insert a document
-await collection. insertOne({ name: "Alice", age: 25 }); 
+await collection. insertOne({ name: "Nurin", age: 22 }); 
 console. log ("Document inserted!");
 
 // Query the document
-const result = await collection. findOne({ name: "Alice" });
+const result = await collection. findOne({ name: "Nurin" });
 console. log("Query result:", result);
 } catch (err) {
 console. error ("Error:", err);
